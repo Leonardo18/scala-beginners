048de6108d6ee592c800cf9c1891a1cc86eddc3c
Leonardo Dutra <leo_dutra18@hotmail.com>
2024-04-02 19:52:02 -0300

    :construction: add file to learn vals and variables types

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,31 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  //Values
+  val x: Int = 42
+  println(x)
+
+  // VALS ARE IMMUTABLE
+  // COMPILER CAN INFER TYPES
+
+  val aString: String = "Mini Ninja Baguncinha"
+  val anotherString: String = "another string"
+
+  val falseBoolean: Boolean = false
+  val trueBoolean: Boolean = true
+
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 499999999
+  val aFloat: Float = 2.0
+  val aDouble: Double = 3.14
+
+  //Variables
+  var aVariable: Int = 4
+
+  aVariable += 5  //side effects
+
+  println(aVariable)
+}
